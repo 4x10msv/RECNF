45f11d95ade4b301738a812f5de449af461fcb71
Nicola115 <2225992901@qq.com>
2020-06-28 12:27:57 +0800

    add window for ouputing process

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ from tkinter import scrolledtext as st
 import horn
 import cnf
 
+
+
+showText = None
+
 '''
     调用horn子句归结程序
     增加窗口用于显示归结过程
@@ -13,25 +17,30 @@ def horn_run_and_get_detail(s):
     #运行horn子句归结
     horn.horn_run(inputText.get('1.0', 'end-1c'))
     
-    #绘制窗口
-    showText = st.ScrolledText(
-        master=window,
-        width='50',
-        height='10'
-    )
-    showText.pack()
-    showText.config(state = NORMAL)
-    #窗口插入归结过程
+    global showText
     
+    if not showText:
+        #绘制窗口
+        showText = st.ScrolledText(
+                master=window,
+                width='50',
+                height='10'
+                )
+        showText.pack()
+    
+    showText.config(state = NORMAL)
+    
+    #窗口插入归结过程
     chars1 = "请在这里放上归结过程"
-    chars2 = "\n测试多行输出"
+    chars2 = "测试多行输出"
     #请在str_list中插入归结过程
     str_list = [chars1,chars2]
     for i in str_list:
-        showText.insert("insert",i)
+        showText.insert("insert",'\n'+i)
     
     #禁止用户输入
     showText.config(state = DISABLED)
+
 
 '''
 题目求解演示程序
@@ -39,14 +48,31 @@ def horn_run_and_get_detail(s):
 progText为过程字符串
 '''
 def runCnfResolution(rawStr):
+    
+    global showText
+    
+    if not showText:
+        #绘制窗口
+        showText = st.ScrolledText(
+                master=window,
+                width='50',
+                height='10'
+                )
+        showText.pack()
     progText = cnf.cnfResolution(rawStr)
+    
     #todo
-    #print(progText)
+    showText.config(state = NORMAL)
+    showText.insert("insert",'\n'+progText)
+    
+    #禁止用户输入
+    showText.config(state = DISABLED)
+    return showText
 
 if __name__ == '__main__':
     window = tk.Tk()
     window.title('归结原理演示程序')
-    window.geometry('800x600')
+    window.geometry('400x300')
 
     inputText = tk.Text(
         master=window,
@@ -54,7 +80,7 @@ if __name__ == '__main__':
         height='10'
     )
     inputText.pack()
-
+    
     runButton = tk.Button(
         master=window,
         text='题目求解',
